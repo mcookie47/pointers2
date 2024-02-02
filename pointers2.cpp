c57c5df5f906acbca77378c4134e3ce2c228c7da
mcookie47 <57654936+mcookie47@users.noreply.github.com>
2024-02-01 23:57:44 -0800

    Add files via upload

---
+++ pointers2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int firstvalue, secondvalue;
+	int* mypointer;
+
+	mypointer = &firstvalue;
+	*mypointer = 10;
+	mypointer = &secondvalue;
+	*mypointer = 20;
+	cout << "firstvalue is " << firstvalue << '\n';
+	cout << "secondvalue is " << secondvalue << '\n';
+
+	system("pause");
+	return 0;
+}
